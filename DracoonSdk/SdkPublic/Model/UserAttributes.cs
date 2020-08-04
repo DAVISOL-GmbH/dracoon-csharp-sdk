7a7fefe8e81ec59b12ce68fee02ec41a57c6bd42
Sebastian Hübner <25882725+shuebner20@users.noreply.github.com>
2020-08-04 11:19:08 +0200

    Fixed last commit

--- DracoonSdk/SdkPublic/Model/UserAttributes.cs
+++ DracoonSdk/SdkPublic/Model/UserAttributes.cs
@@ -3,6 +3,9 @@ using System.Linq;
 
 namespace Dracoon.Sdk.Model {
     public class UserAttributes : SimpleListBase<KeyValuePair<string, string>> {
+
+        public UserAttributes() { }
+
         public UserAttributes(IEnumerable<KeyValuePair<string, string>> items) {
             Items = items.ToArray();
         }
