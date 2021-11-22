6e2f6d4c62343d442e61ef8486bb04be2acf066e
Sebastian Hübner <25882725+shuebner20@users.noreply.github.com>
2021-11-22 15:50:24 +0100

    extend UserRoles (add nonmember viewer role)

--- DracoonSdk/SdkPublic/Model/UserRole.cs
+++ DracoonSdk/SdkPublic/Model/UserRole.cs
@@ -2,11 +2,18 @@
     /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/UserRole/*'/>
     public enum UserRole {
         /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/Undefined/*'/>
-        Undefined, /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/ConfigManager/*'/>
-        ConfigManager, /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/UserManager/*'/>
-        UserManager, /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/GroupManager/*'/>
-        GroupManager, /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/RoomManager/*'/>
-        RoomManager, /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/LogAuditor/*'/>
-        LogAuditor
+        Undefined = 0,
+        /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/ConfigManager/*'/>
+        ConfigManager = 1,
+        /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/UserManager/*'/>
+        UserManager = 2,
+        /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/GroupManager/*'/>
+        GroupManager = 3,
+        /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/RoomManager/*'/>
+        RoomManager = 4,
+        /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/LogAuditor/*'/>
+        LogAuditor = 5,
+        /// <include file = "ModelDoc.xml" path='docs/members[@name="userRole"]/NonmemberViewer/*'/>
+        NonmemberViewer = 6
     }
 }
