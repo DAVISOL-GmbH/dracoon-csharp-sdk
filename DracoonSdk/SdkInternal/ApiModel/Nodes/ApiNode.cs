9b9aa61903563c19c046bccc6608def07c46140a
Sebastian Hübner <25882725+shuebner20@users.noreply.github.com>
2023-11-05 17:40:23 +0100

    fix duplicate using statement

--- DracoonSdk/SdkInternal/ApiModel/Nodes/ApiNode.cs
+++ DracoonSdk/SdkInternal/ApiModel/Nodes/ApiNode.cs
@@ -1,7 +1,6 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System;
-using System;
 
 namespace Dracoon.Sdk.SdkInternal.ApiModel {
     internal class ApiNode : ApiTrackExternalModification {
